801a61dfb35a38447fdd85f7085f3f232e49ce22
mrozi <mrozi.m@gmail.com>
2019-01-26 20:42:09 +0100

    Change in database

--- structure.sql
+++ structure.sql
@@ -7,16 +7,18 @@ create 'dewey_files' , 'description' , 'params', 'history'
 create external table dewey_users (
 key string,
 login string,
+password string,
 name string,
 surname string,
 is_add  boolean,
 is_change boolean,
 is_delete boolean,
-operation_date timestamp
+operation_date timestamp,
+orderby bigint
 )
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' 
 WITH SERDEPROPERTIES 
-("hbase.columns.mapping" =":key,description:login,description:name,description:surname,params:is_add, params:is_change, params:is_delete,history:operation_date")
+("hbase.columns.mapping" =":key,description:login,description:password,description:name,description:surname,params:is_add, params:is_change, params:is_delete,history:operation_date,history:orderby")
 TBLPROPERTIES("hbase.table.name" = "dewey_users")
 
 
@@ -29,11 +31,12 @@ is_add boolean,
 is_change boolean,
 is_delete boolean,
 is_clone boolean,
-operation_date timestamp
+operation_date timestamp,
+orderby bigint
 )
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' 
 WITH SERDEPROPERTIES 
-("hbase.columns.mapping" =":key,description:filename,description:extension,params:parent, params:is_add, params:is_change,params:is_delete,params:is_clone, history:operation_date")
+("hbase.columns.mapping" =":key,description:filename,description:extension,params:parent, params:is_add, params:is_change,params:is_delete,params:is_clone,history:operation_date,history:orderby")
 TBLPROPERTIES("hbase.table.name" = "dewey_files")
 
 
